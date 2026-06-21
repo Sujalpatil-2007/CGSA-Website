f483671f9fd8b63be5d6823ac85fbc19c761df2a
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-06-21 13:49:23 +0530

    bug fixed

--- Frontend/src/features/Article/components/publishForm.jsx
+++ Frontend/src/features/Article/components/publishForm.jsx
@@ -1,7 +1,8 @@
 import React, { useState } from "react";
 import { createArticle } from "../services/Article.api";
-import {Send} from 'lucide-react'
+import { Send } from "lucide-react";
 import { useNavigate } from "react-router";
+import {toast} from 'react-toastify'
 
 const publishForm = () => {
   const navigate = useNavigate();
@@ -24,22 +25,34 @@ const publishForm = () => {
   const handleSubmit = async (e) => {
     e.preventDefault();
 
-    try {
-      if (window.confirm("Do you want to publish it?")) {
-      const data = await createArticle(formData);
-      setFormData({
-        title: "",
-        category: "",
-        description: "",
-        image: "",
-        content: "",
-        author: "",
-      });
-      navigate(`/articles/${data.article._id}`);
-    }} catch (err) {
-      console.log(err);
+    if (
+      formData.title !== "" &&
+      formData.description !== "" &&
+      formData.author !== "" &&
+      formData.content !== "" &&
+      formData.category !== "" &&
+      formData.image !== ""
+    )
+      try {
+        if (window.confirm("Do you want to publish it?")) {
+          const data = await createArticle(formData);
+          setFormData({
+            title: "",
+            category: "",
+            description: "",
+            image: "",
+            content: "",
+            author: "",
+          });
+          navigate(`/articles/${data.article._id}`);
+        }
+      } catch (err) {
+        console.log(err);
+      }
+    else{
+      toast.error("Please fill all fields ❌");
     }
-};
+  };
 
   return (
     <main className="h-full w-full dark:bg-gray-900   flex justify-center items-center ">
@@ -61,140 +74,154 @@ const publishForm = () => {
         </div>
         <div className=" lg:h-[90%] w-full ">
           <div className="h-full w-full rounded-2xl border-2 border-[#aba69ca7]  ">
-            <form onSubmit={handleSubmit} className="h-full dark:bg-gray-800 dark:text-white p-3 w-full">
+            <form
+              onSubmit={handleSubmit}
+              className="h-full dark:bg-gray-800 dark:text-white p-3 w-full"
+            >
               {/* Title */}
               <div>
-                <label
-                htmlFor="title"
-                className="flex text font-[font2] "
-              >Title <span className="text-red-500">*</span> </label>
-              <input
-                className=" rounded w-full  p-1 border-2 border-gray-400"
-                type="text"
-                id="title"
-                name="title"
-                value={formData.title}
-                onChange={handleChange}
-                required
-                placeholder="Enter article title"
-              />
+                <label htmlFor="title" className="flex text font-[font2] ">
+                  Title <span className="text-red-500">*</span>{" "}
+                </label>
+                <input
+                  className=" rounded w-full  p-1 border-2 border-gray-400"
+                  type="text"
+                  id="title"
+                  name="title"
+                  value={formData.title}
+                  onChange={handleChange}
+                  required
+                  placeholder="Enter article title"
+                />
               </div>
               {/* img and category  */}
               <div className="flex flex-col    w-full   ">
-              <div className="flex gap-10 relative ">
-                {/* image url */}
-              <div>
-                <label
-                htmlFor="image"
-                className="flex text font-[font2] "
-              >Image <span className="text-red-500">*</span> </label>
-              <input
-                className=" lg:w-96 w-42  rounded p-1 border-2 border-gray-400"
-                type="text"
-                id="image"
-                name="image"
-                value={formData.image}
-                onChange={handleChange}
-                required
-                placeholder="https://example.com/image.jpg"
-              />
+                <div className="flex gap-10 relative ">
+                  {/* image url */}
+                  <div>
+                    <label htmlFor="image" className="flex text font-[font2] ">
+                      Image <span className="text-red-500">*</span>{" "}
+                    </label>
+                    <input
+                      className=" lg:w-96 w-42  rounded p-1 border-2 border-gray-400"
+                      type="text"
+                      id="image"
+                      name="image"
+                      value={formData.image}
+                      onChange={handleChange}
+                      required
+                      placeholder="https://example.com/image.jpg"
+                    />
+                  </div>
+                  {/* category */}
+                  <div>
+                    <label
+                      htmlFor="category"
+                      className="flex h-5 w-5  text font-[font2] "
+                    >
+                      Category <span className="text-red-500">*</span>
+                    </label>
+                    <select
+                      name="category"
+                      id="category"
+                      value={formData.category}
+                      onChange={handleChange}
+                      className="bg-[#cac5bb68] dark:bg-gray-700 rounded "
+                      required
+                    >
+                      <option value="">Select a category</option>
+                      <option value="Truth">Truth</option>
+                      <option value="Non-Violence">Non-Violence</option>
+                      <option value="Simplicity">Simplicity</option>
+                      <option value="Service">Service</option>
+                    </select>
+                  </div>
+                  {/* img */}
+
+                  {formData.image && (
+                    <div className="h-32 w-52 mt-1 flex gap-3 absolute left-150 ">
+                      <img
+                        className="bg-[#938a8a51] object-cover rounded "
+                        src={formData.image}
+                        alt="Image Preview"
+                      />
+                      <p className="font-[font1] w-56 whitespace-nowrap ">
+                        Paste an image URL to <br /> preview your article cover
+                        image.
+                      </p>
+                    </div>
+                  )}
+                </div>
+                {/* Author */}
+                <div>
+                  <label htmlFor="author" className="flex text font-[font2] ">
+                    Author <span className="text-red-500">*</span>{" "}
+                  </label>
+                  <input
+                    className=" rounded p-1 border-2 border-gray-400"
+                    type="text"
+                    id="author"
+                    name="author"
+                    value={formData.author}
+                    onChange={handleChange}
+                    required
+                    placeholder="Author name"
+                  />
+                </div>
               </div>
-              {/* category */}
-              <div>
-              <label
-                htmlFor="category"
-                className="flex h-5 w-5  text font-[font2] "
-              >Category <span className="text-red-500">*</span></label>
-              <select name="category"
-               id="category"
-               value={formData.category}
-               onChange={handleChange}
-                className="bg-[#cac5bb68] dark:bg-gray-700 rounded "
-               required
-               >
-                <option value="">Select a category</option>
-                <option value="Truth">Truth</option>
-                <option value="Non-Violence">Non-Violence</option>
-                <option value="Simplicity">Simplicity</option>
-                <option value="Service">Service</option>
-               </select>
-              </div>
-              {/* img */}
-              
-              {formData.image && (
-                <div className="h-32 w-52 mt-1 flex gap-3 absolute left-150 ">
-                <img className="bg-[#938a8a51] object-cover rounded " src={formData.image} alt="Image Preview" />
-                <p className="font-[font1] w-56 whitespace-nowrap " >Paste an image URL to <br /> preview your article cover image.</p>
-              </div>
-              )}
-              
-              </div>
-              {/* Author */}
-              <div>
-                <label
-                htmlFor="author"
-                className="flex text font-[font2] "
-              >Author <span className="text-red-500">*</span> </label>
-              <input
-                className=" rounded p-1 border-2 border-gray-400"
-                type="text"
-                id="author"
-                name="author"
-                value={formData.author}
-                onChange={handleChange}
-                required
-                placeholder="Author name"
-              />
-              </div>
-              </div>
-            
-              
+
               {/* description */}
               <div>
                 <label
-                htmlFor="description"
-                className="flex text font-[font2] "
-              >Description <span className="text-red-500">*</span> </label>
-              <textarea
-                className=" rounded w-full p-1 border-2 border-gray-400"
-                type="text"
-                id="description"
-                name="description"
-                value={formData.description}
-                rows="3"
-                onChange={handleChange}
-                required
-                placeholder="Short article description..."
-              />
+                  htmlFor="description"
+                  className="flex text font-[font2] "
+                >
+                  Description <span className="text-red-500">*</span>{" "}
+                </label>
+                <textarea
+                  className=" rounded w-full p-1 border-2 border-gray-400"
+                  type="text"
+                  id="description"
+                  name="description"
+                  value={formData.description}
+                  rows="3"
+                  onChange={handleChange}
+                  required
+                  placeholder="Short article description..."
+                />
               </div>
               {/* content */}
               <div>
-                <label
-                htmlFor="content"
-                className="flex text font-[font2] "
-              >Content <span className="text-red-500">*</span> </label>
-              <textarea
-                className=" rounded w-full p-1 border-2 border-gray-400"
-                type="text"
-                id="content"
-                name="content"
-                value={formData.content}
-                rows="9"
-                onChange={handleChange}
-                required
-                placeholder="Write your article content..."
-              />
+                <label htmlFor="content" className="flex text font-[font2] ">
+                  Content <span className="text-red-500">*</span>{" "}
+                </label>
+                <textarea
+                  className=" rounded w-full p-1 border-2 border-gray-400"
+                  type="text"
+                  id="content"
+                  name="content"
+                  value={formData.content}
+                  rows="9"
+                  onChange={handleChange}
+                  required
+                  placeholder="Write your article content..."
+                />
               </div>
-              
+
               <div className="pt-1">
-                <button className="bg-red-400  flex justify-center items-center cursor-pointer active:scale-98 rounded px-3 py-2 left-0 font-[font2] " onClick={handleSubmit}><Send size={30}  className="p-1" />Publish Article</button>
+                <button
+                  className="bg-red-400  flex justify-center items-center cursor-pointer active:scale-98 rounded px-3 py-2 left-0 font-[font2] "
+                  onClick={handleSubmit}
+                >
+                  <Send size={30} className="p-1" />
+                  Publish Article
+                </button>
               </div>
             </form>
           </div>
         </div>
       </div>
     </main>
-    
   );
 };
 
