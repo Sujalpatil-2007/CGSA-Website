44b222de2fe8421fe342ec94e9e10aba32ba00a1
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-06-22 22:42:52 +0530

    proper structure

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,9 @@
-import React from 'react'
-import { RouterProvider } from 'react-router'
-import { router } from './app.routes.jsx'
+import React from "react";
+import { RouterProvider } from "react-router";
+import { router } from "./app.routes.jsx";
 
 const App = () => {
-  return (
-    <RouterProvider router={router} />
-  )
-}
+  return <RouterProvider router={router} />;
+};
 
-export default App
+export default App;
