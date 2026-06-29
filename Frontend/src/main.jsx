97edb7e19641537b02eb83b48c3310bd6ddb1ec5
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-06-29 23:50:38 +0530

    main clean

--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,22 +1,18 @@
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
-import { AuthProvider } from './features/auth/auth.context.jsx'
-import { ToastContainer } from 'react-toastify'
-import 'react-toastify/dist/ReactToastify.css'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { AuthProvider } from "./features/auth/auth.context.jsx";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 
-
-createRoot(document.getElementById('root')).render(
-    <AuthProvider>
-        <App/>
-        <ToastContainer
+createRoot(document.getElementById("root")).render(
+  <AuthProvider>
+    <App />
+    <ToastContainer
       position="top-right"
       autoClose={3000}
       draggable
-  draggableDirection="x"
-      
+      draggableDirection="x"
     />
-    </AuthProvider>
-        
-)
-
+  </AuthProvider>,
+);
