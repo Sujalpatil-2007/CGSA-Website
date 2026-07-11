375005fe65929b1b1f64abeaa0d858c3d2b63a90
Sujalpatil-2007 <patilsujalvr4@gmail.com>
2026-07-11 16:51:01 +0530

    protected admin routes

--- Frontend/src/features/admin/components/AdminProtected.jsx
+++ Frontend/src/features/admin/components/AdminProtected.jsx
@@ -2,13 +2,15 @@ import { Navigate } from "react-router";
 import { useContext } from "react";
 import { AuthContext } from "../../auth/auth.context";
 import Loading from "../../auth/pages/Loading";
+import { toast } from "react-toastify";
 
 const AdminProtected = ({ children }) => {
   const { user, loading } = useContext(AuthContext);
 
 
   if (!user) {
-    return <Navigate to="/login" replace />;
+    return <Navigate to="/" replace />;
+    toast.error("Not login ")
   }
 
   if (!user.isSuperAdmin) {
